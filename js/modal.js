1c16a9b87232da67e0cbc3acbf7bf1741cb4f7f3
BDODINKA <bdodinka1992@gmail.com>
2020-07-15 20:09:56 +0300

    second

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,9 @@
 $(function(){
-    $.ajaxsetup(e);{
-        type:'POST',
-        error. Function ();{
-              Function ();{
-         modal.append('<img id="loader" src="/media/img/loader.gif"/>')
-    }
-
-let fx ={
+    let fx ={
     'initmodal':function() {
         if ($('.modal-window').length == 0) {
-           $('<div>').addClass(t,'overlay').appendTo('body');
-            return $('<div>').addClass(t ,'modal-window').appendTo('body');
+           $('<div>').addClass('overlay').appendTo('body');
+            return $('<div>').addClass('modal-window').appendTo('body');
         
     } else {
         return $('.modal-window');
@@ -33,6 +26,9 @@ $('<a>').html('.&times;').addClass(t ,'close').attr('href','#').click(function(e
             success: function(data) {
                 modal.append(data);
                 $('#loader').hide();
-            }
+            
+            error:function(msg){
+                console.log(msg);
+               }
         });
     });
